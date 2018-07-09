1308ded641d9f057713294f39424b52d3214f60f
uziam <ahmad.uzair@outlook.com>
2018-07-08 21:50:05 -0400

    - Fixed indentation and changed tabs to spaces - Fixed other things regarding the coding style - Changed some variables names

--- frac_divider.vhd
+++ frac_divider.vhd
@@ -2,94 +2,97 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity frac_divider is
-	generic(	M 		: integer;
-			N 		: integer;
-			x 		: integer);
-	port(		reset	: in std_logic;
-			clk_in	: in std_logic;
-			clk_out	: out std_logic);
+    generic (
+        M           : integer;
+        N           : integer;
+        x           : integer
+    );
+    port (
+        rst         : in std_logic;
+        clk_i       : in std_logic;
+        clk_o       : out std_logic
+    );
 end frac_divider;
 
 architecture behavior of frac_divider is
-	-- since there is a divide-by-two circuit at the output
-	-- multiply N by 2, to cancel the effect
-	constant P: integer := N*2; -- use this instead of N
+    -- since there is a divide-by-two circuit at the output
+    -- multiply N by 2, to cancel the effect
+    constant P: integer         := N*2; -- use this instead of N
 
-	-- calculate the required constants
-	constant L: integer := integer(M/P) + 1;
-	constant J: integer := L - 1;
-	constant C: integer := x * P;
-	constant B: integer := C*L - C*M/P;
+    -- calculate the required constants
+    constant L: integer         := integer(M/P) + 1;
+    constant J: integer         := L - 1;
+    constant C: integer         := x * P;
+    constant B: integer         := C*L - C*M/P;
 
-	-- internal signals
-	signal cycle_counter	: integer range 0 to C-1;
-	signal modulus			: integer range 0 to L-1;
-	signal mod_counter		: integer range 0 to L-1;
-	signal mod_flag			: std_logic; -- flag raised when mod_counter reaches 0
-	signal out_reg			: std_logic; -- output register
-
+    -- internal signals
+    signal cycle_counter        : integer range 0 to C-1;
+    signal modulus              : integer range 0 to L-1;
+    signal mod_counter          : integer range 0 to L-1;
+    signal mod_flag             : std_logic; -- raised when mod_counter is 0
+    signal out_reg              : std_logic; -- output register
 begin
-	-- this is the cycle counter, counts from C-1 downto 0
-	-- count decrements everytime mod_flag is raised
-	-- goes back to C-1 after reaching 0
-	-- synchoronus reset
-	p_cycle_count: process(clk_in)
-	begin
-		if(rising_edge(clk_in)) then
-			if(reset = '1') then
-				cycle_counter <= 0;
-			else
-				if(mod_flag = '1') then
-					if(cycle_counter = 0) then
-						cycle_counter <= C-1; -- reset count
-					else
-						cycle_counter <= cycle_counter - 1;
-					end if;
-				end if;
-			end if;
-		end if;
-	end process;
+    -- output clock
+    clk_o   <= out_reg;
 
-	-- mux to select between J and L based on B (0..B will be J, then B..C = A will be L)
-	modulus <= (J-1) when (cycle_counter < B) else (L-1);
+    -- this is the cycle counter, counts from C-1 downto 0
+    -- count decrements everytime mod_flag is raised
+    -- goes back to C-1 after reaching 0
+    -- synchoronus rst
+    p_cycle_count: process (clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if (rst = '1') then
+                cycle_counter <= 0;
+            else
+                if (mod_flag = '1') then
+                    if (cycle_counter = 0) then
+                        cycle_counter <= C-1; -- rst count
+                    else
+                        cycle_counter <= cycle_counter - 1;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
 
-	-- this is the dual-modulus counter
-	-- counts from modulus downto 0
-	-- updates counter to current modulus everytime count reaches 0
-	-- synchronus reset
-	p_mod_count: process(clk_in)
-	begin
-		if(rising_edge(clk_in)) then
-			if(reset = '1') then
-				mod_counter <= modulus;
-			else
-				if(mod_flag = '1') then
-					mod_counter <= modulus;
-				else
-					mod_counter <= mod_counter - 1;
-				end if;
-			end if;
-		end if;
-	end process;
+    -- mux to select between J and L based on B (0..B will be J,
+    -- then B..C = A will be L)
+    modulus <= (J-1) when (cycle_counter < B) else (L-1);
 
-	-- raise the flag if mod_counter reaches 0
-	-- this is the ouput but with twice the frequency required
-	mod_flag <= '1' when(mod_counter = 0) else '0';
+    -- this is the dual-modulus counter
+    -- counts from modulus downto 0
+    -- updates counter to current modulus everytime count reaches 0
+    -- synchronus rst
+    p_mod_count: process (clk_i)
+    begin
+        if rising_edge (clk_i) then
+            if (rst = '1') then
+                mod_counter <= modulus;
+            else
+                if (mod_flag = '1') then
+                    mod_counter <= modulus;
+                else
+                    mod_counter <= mod_counter - 1;
+                end if;
+            end if;
+        end if;
+    end process;
 
-	-- this is a divide-by-two counter
-	-- divides mod_flag frequency by 2
-	p_div_by_2: process(clk_in)
-	begin
-		if(rising_edge(clk_in)) then
-			if(reset = '1') then
-				out_reg <= '0';
-			elsif(mod_flag = '1') then
-				out_reg <= not out_reg;
-			end if;
-		end if;
-	end process;
+    -- raise the flag if mod_counter reaches 0
+    -- this is the ouput but with twice the frequency required
+    mod_flag <= '1' when (mod_counter = 0) else '0';
 
-	-- output clock
-	clk_out <= out_reg;
-
-end behavior; 
+    -- this is a divide-by-two counter
+    -- divides mod_flag frequency by 2
+    p_div_by_2: process (clk_i)
+    begin
+        if rising_edge(clk_i) then
+            if (rst = '1') then
+                out_reg <= '0';
+            elsif (mod_flag = '1') then
+                out_reg <= not out_reg;
+            end if;
+        end if;
+    end process;
+end behavior;
--- frac_divider_tb.vhd
+++ frac_divider_tb.vhd
@@ -5,78 +5,66 @@ entity frac_divider_tb is
 end frac_divider_tb;
 
 architecture behavior of frac_divider_tb is
+    -- output clock signal
+    signal rst          : std_logic := '0';
+    signal clk_i        : std_logic := '0';
+    signal clk_o        : std_logic;
 
-	-- output clock signal
-	signal clk_out 	: std_logic;
+    -- clock configuration
+    constant clk_period : time := 20 ns; -- f = 50 MHz
+    signal finished     : std_logic := '0';
 
-	-- reset signal
-	signal reset	: std_logic := '0';
-
-	-- input clock signal
-	signal clk_in 	: std_logic := '0';
-
-	-- clock confg
-	constant clk_period : time := 20 ns; -- f = 50 MHz
-	signal finished 	: std_logic := '0';
-
-	-- clock cycle counts
-	signal count_in		: integer := 0; -- for clk_in
-	signal count_out	: integer := 0; -- for clk_out
-
-	-- fractional divider component
-	component frac_divider is
-		generic(	M 		: integer;
-					N 		: integer;
-					x 		: integer);
-		port(		reset	: in std_logic;
-					clk_in	: in std_logic;
-					clk_out	: out std_logic);
-	end component;
-
+    -- clock cycle counts
+    signal count_in     : integer := 0;
+    signal count_out    : integer := 0;
 begin
-	-- count the number of clock cycles in clk_in
-	process(clk_in)
-	begin
-		if(reset = '0') then
-			if(rising_edge(clk_in)) then
-				count_in <= count_in + 1;
-			end if;
-		end if;
-	end process;
+    -- count the number of clock cycles in clk_i
+    process (clk_i)
+    begin
+        if (rst = '0') then
+            if rising_edge(clk_i) then
+                count_in <= count_in + 1;
+            end if;
+        end if;
+    end process;
 
-	-- count the number of clock cycles in clk_out
-	process(clk_out)
-	begin
-		if(reset = '0') then
-			if(rising_edge(clk_out)) then
-				count_out <= count_out + 1;
-			end if;
-		end if;
-	end process;
+    -- count the number of clock cycles in clk_o
+    process (clk_o)
+    begin
+        if (rst = '0') then
+            if rising_edge(clk_o) then
+                count_out <= count_out + 1;
+            end if;
+        end if;
+    end process;
 
-	-- unit under test
-	fd 	: frac_divider 
-			-- M=25 and N=3 to get clk_out = 6 MHz
-			generic map(M 		=> 25,
-						N 		=> 3,
-						x		=> 10)
-			port map(	reset 	=> reset,
-						clk_in 	=> clk_in,
-						clk_out	=> clk_out);
+    -- unit under test
+    u_frac_divider  : entity work.frac_divider 
+        generic map (
+            -- M=25 and N=3 to get clk_o = 6 MHz
+            M       => 25,
+            N       => 3,
+            x       => 10
+        )
+        port map (
+            rst     => rst,
+            clk_i   => clk_i,
+            clk_o   => clk_o
+        );
 
-	-- generate clock
-	clk_in <= (not clk_in) after clk_period/2 when finished /= '1' else '0';
+    -- generate clock
+    clk_i <= (not clk_i) after clk_period/2 when finished /= '1' else '0';
 
-	-- testing
-	process
-	begin
-		reset <= '1';
-		wait for (10 * clk_period);
-		reset <= '0';
-		wait for (100000 * clk_period);
-		finished <= '1';
-		report "Output Frequency = " & real'image(real(real(count_out)*50.0/real(count_in))) & " MHz";
-		wait;
-	end process;
-
+    -- testing
+    process
+    begin
+        rst <= '1';
+        wait for (10 * clk_period);
+        rst <= '0';
+        wait for (100000 * clk_period);
+        finished <= '1';
+        report "Output Frequency = " & real'image(real(real(count_out) * 50.0 /
+                                        real(count_in))) & " MHz";
+        wait;
+    end process;
 end architecture;
